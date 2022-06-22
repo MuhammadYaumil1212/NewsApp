ee7502cf8ba367f65d019294ac0dffff632dd424
yaumil <yaumilramkhadani@gmail.com>
2022-06-22 13:10:40 +0700

    activity main binding

--- app/src/main/java/com/edelweiss/edelnews/ui/MainActivity.kt
+++ app/src/main/java/com/edelweiss/edelnews/ui/MainActivity.kt
@@ -2,25 +2,29 @@ package com.edelweiss.edelnews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.edelweiss.edelnews.R
 import com.edelweiss.edelnews.data.NewsData
+import com.edelweiss.edelnews.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var navHostFragment: NavHostFragment
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupNavigation()
     }
 
     private fun setupNavigation() {
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
                 as NavHostFragment
-        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView = binding.bottomNavigationView
         NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController)
     }
 }
